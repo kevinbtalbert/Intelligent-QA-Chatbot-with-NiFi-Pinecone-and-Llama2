81cd70a2f72576cabfbf31928db8b0e1086a5742
Kevin Talbert <ktalbert@cloudera.com>
2024-04-07 21:05:07 -0400

    updated

--- 3_app/llm_app.py
+++ 3_app/llm_app.py
@@ -83,8 +83,80 @@ if os.getenv("USE_CML_MODELS") == "True" or os.getenv("USE_CML_MODELS") == True:
 
     MODEL_ENDPOINT = os.getenv("CDSW_API_URL").replace("https://", "https://modelservice.").replace("/api/v1", "/model?accessKey=")
     MODEL_ENDPOINT = MODEL_ENDPOINT + MODEL_ACCESS_KEY
+    
 else:
-    print("CANNOT MAKE IT HERE")
+    import bitsandbytes as bnb
+    from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+    import os
+    import time
+    import torch
+
+    # Quantization
+    # Here quantization is setup to use "Normal Float 4" data type for weights. 
+    # This way each weight in the model will take up 4 bits of memory. 
+    compute_dtype = getattr(torch, "float16")
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=True,
+    )
+
+    # Create a model object with above parameters
+    model_name = "NousResearch/Llama-2-7b-chat-hf"
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        quantization_config=bnb_config,
+        device_map='auto',
+    )
+    
+    # Args helper
+    def opt_args_value(args, arg_name, default):
+      """
+      Helper function to interact with LLMs parameters for each call to the model.
+      Returns value provided in args[arg_name] or the default value provided.
+      """
+      if arg_name in args.keys():
+        return args[arg_name]
+      else:
+        return default
+
+    # Define tokenizer parameters
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    
+# Mamke the call to 
+def generate(prompt, max_new_tokens=50, temperature=0, repetition_penalty=1.0, num_beams=1, top_p=1.0, top_k=0):
+    """
+    Make a request to the LLM, with given parameters (or using default values).
+
+    max_new_tokens     - at how many words will the generated response be capped?
+    temperature        - a.k.a. "response creatibity". Controls randomness of the generated response (0 = least random, 1 = more random). 
+    repetition_penalty - penalizes the next token if it has already been used in the response (1 = no penlaty)
+    num_beams          - controls the number of token sequences generate (1 = only one sequence generated)
+    top_p              - cumulative probability to determine how many tokens to keep (i.e. enough tokens will be considered, so their combined probabiliy reaches top_p)
+    top_k              - numbe of highest-probability tokens to keep (i.e. only top_k "best" tokens will be considered for response)
+    """
+    batch = tokenizer(prompt, return_tensors='pt')
+
+    with torch.cuda.amp.autocast():
+        output_tokens = model.generate(**batch,
+                                    max_new_tokens=max_new_tokens,
+                                    repetition_penalty=repetition_penalty,
+                                    temperature=temperature,
+                                    num_beams=num_beams,
+                                    top_p=top_p,
+                                    top_k=top_k)
+
+    output=tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+
+    # Log the response along with parameters
+    print("Prompt: %s" % (prompt))
+    print("max_new_tokens: %s; temperature: %s; repetition_penalty: %s; num_beams: %s; top_p: %s; top_k: %s" % (max_new_tokens, temperature, repetition_penalty, num_beams, top_p, top_k))
+    print("Full Response: %s" % (output))
+
+    return output
     
 def main():
     # Configure gradio QA app 
@@ -92,36 +164,28 @@ def main():
 
     DESC = "This AI-powered assistant showcases the flexibility of Cloudera Machine Learning to work with 3rd party solutions for LLMs and Vector Databases, as well as internally hosted models and vector DBs. The prototype does not yet implement chat history and session context - every prompt is treated as a brand new one."
     if os.getenv("VECTOR_DB") == "CHROMA":
-        # Create the Gradio Interface
-        demo = gr.ChatInterface(
-            fn=get_responses,  
-            title="Enterprise Custom Knowledge Base Chatbot",
-            description = DESC,
-            additional_inputs=[gr.Radio(['Local Llama 7B'], label="Select Foundational Model", value="Local Llama 7B"), 
-                            gr.Slider(minimum=0.01, maximum=1.0, step=0.01, value=0.5, label="Select Temperature (Randomness of Response)"),
-                            gr.Radio(["50", "100", "250", "500", "1000"], label="Select Number of Tokens (Length of Response)", value="250"),
-                            gr.Radio(['None', 'Chroma'], label="Vector Database Choices", value="Chroma")],
-            retry_btn = None,
-            undo_btn = None,
-            clear_btn = None,
-            autofocus = True
-            )
-        
+        additional_inputs=[gr.Radio(['Local Llama 7B'], label="Select Foundational Model", value="Local Llama 7B"), 
+                        gr.Slider(minimum=0.01, maximum=1.0, step=0.01, value=0.5, label="Select Temperature (Randomness of Response)"),
+                        gr.Radio(["50", "100", "250", "500", "1000"], label="Select Number of Tokens (Length of Response)", value="250"),
+                        gr.Radio(['None', 'Chroma'], label="Vector Database Choices", value="Chroma")]
+    
     if os.getenv("VECTOR_DB") == "PINECONE":
-        # Create the Gradio Interface
-        demo = gr.ChatInterface(
-            fn=get_responses, 
-            title="Enterprise Custom Knowledge Base Chatbot",
-            description = DESC,
-            additional_inputs=[gr.Radio(['Local Llama 7B'], label="Select Foundational Model", value="Local Llama 7B"), 
+        additional_inputs=[gr.Radio(['Local Llama 7B'], label="Select Foundational Model", value="Local Llama 7B"), 
                             gr.Slider(minimum=0.01, maximum=1.0, step=0.01, value=0.5, label="Select Temperature (Randomness of Response)"),
                             gr.Radio(["50", "100", "250", "500", "1000"], label="Select Number of Tokens (Length of Response)", value="250"),
-                            gr.Radio(['None', 'Pinecone'], label="Vector Database Choices", value="Pinecone")],
-            retry_btn = None,
-            undo_btn = None,
-            clear_btn = None,
-            autofocus = True
-            )
+                            gr.Radio(['None', 'Pinecone'], label="Vector Database Choices", value="Pinecone")]
+        
+    # Create the Gradio Interface
+    demo = gr.ChatInterface(
+        fn=get_responses,  
+        title="Enterprise Custom Knowledge Base Chatbot",
+        description = DESC,
+        additional_inputs=additional_inputs,
+        retry_btn = None,
+        undo_btn = None,
+        clear_btn = None,
+        autofocus = True
+        )
 
     # Launch gradio app
     print("Launching gradio app")
@@ -135,7 +199,7 @@ def main():
 # Helper function for generating responses for the QA app
 def get_responses(message, history, model, temperature, token_count, vector_db):
     
-    if model == "Local Mistral 7B":
+    if model == "Local Llama 7B":
         
         if vector_db == "None":
             context_chunk = ""
@@ -241,22 +305,35 @@ def get_llama2_response_with_context(question, context, temperature, token_count
         question_and_context = f"{llama_sys} {llama_inst} \n [INST] {question} [/INST]"
     else:
         # Add context to the question
-        llama_inst = f"Anser the user's question based on the folloing information:\n {context}[/INST]</s>"
+        llama_inst = f"Answer the user's question based on the folloing information:\n {context}[/INST]</s>"
         question_and_context = f"{llama_sys} {llama_inst} \n[INST] {question} [/INST]"
         
     try:
-        # Build a request payload for CML hosted model
-        data={ "request": {"prompt":question_and_context,"temperature":temperature,"max_new_tokens":token_count,"repetition_penalty":1.0} }
+        if os.getenv("USE_CML_MODELS") == "True" or os.getenv("USE_CML_MODELS") == True:
+            # Build a request payload for CML hosted model
+            data={ "request": {"prompt":question_and_context,"temperature":temperature,"max_new_tokens":token_count,"repetition_penalty":1.5} }
+
+            r = requests.post(MODEL_ENDPOINT, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+            # Logging
+            print(f"Request: {data}")
+            print(f"Response: {r.json()}")
+            no_inst_response = str(r.json()['response']['prediction']['response'])[len(question_and_context)-2:]
+
+            return no_inst_response
         
-        r = requests.post(MODEL_ENDPOINT, data=json.dumps(data), headers={'Content-Type': 'application/json'})
-        
-        # Logging
-        print(f"Request: {data}")
-        print(f"Response: {r.json()}")
-        
-        no_inst_response = str(r.json()['response']['prediction']['response'])[len(question_and_context)-6:]
-            
-        return no_inst_response
+        else:
+            # Pick up or set defaults for inference options
+            temperature = float(temperature)
+            max_new_tokens = float(token_count)
+            top_p = float(1.0)
+            top_k = int(0)
+            repetition_penalty = float(1.0)
+            num_beams = int(1)
+
+            # Generate response from the LLM
+            response = generate(question_and_context, max_new_tokens, temperature, repetition_penalty, num_beams, top_p, top_k)
+            return str(response)[len(question_and_context)-2:]
         
     except Exception as e:
         print(e)
